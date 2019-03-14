a3d0dc21148deb3fa109cd2023da3cddfa7bc08e
henry2992 <henry2992@hotmail.com>
2019-03-13 21:38:39 -0500

    Finish MELI integration

--- app/controllers/retailers/integrations_controller.rb
+++ app/controllers/retailers/integrations_controller.rb
@@ -1,44 +1,27 @@
 class Retailers::IntegrationsController < RetailersController
+  before_action :set_ml, only: [:connect_to_ml]
 
   def index
   end
 
   def connect_to_ml
-    code = params[:code]
-    # 
-
-    url = 'https://api.mercadolibre.com/oauth/token?grant_type=authorization_code'
-    url += "&client_id=#{ENV['MERCADO_LIBRE_ID']}&client_secret=#{ENV['MERCADO_LIBRE_KEY']}"
-    url += "&code=#{code}"
-    url += "&redirect_uri=https://5906e244.ngrok.io/retailers/integrations/mercadolibre"
-
-
-    conn = Faraday.new(:url => url) do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to $stdout
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
-
-    response = conn.post     # GET http://sushi.com/nigiri/sake.json
-    parsed_response = JSON.parse(response.body)
-
-
+    response = get_ml_access_token(params[:code])
     if response.status == 200
-      retailer = current_retailer_user.retailer
-
-      meli_info = retailer.meli_info ? retailer.meli_info : MeliInfo.new(retailer: retailer)
-
-      meli_info.access_token = parsed_response['access_token']
-      meli_info.meli_user_id = parsed_response['user_id']
-      meli_info.refresh_token = parsed_response['refresh_token']
-      meli_info.save
+      @ml.save_access_token(JSON.parse(response.body))
       redirect_to retailers_integrations_path, notice: 'Conectado existosamente'
-
     else
       redirect_to retailers_integrations_path, notice: 'Error al conectarse'
     end
+  end
 
+  private
 
+  def get_ml_access_token(code)
+    @ml.get_access_token_from_url(code)
+  end
+
+  def set_ml
+    @ml = MercadoLibre.new(current_retailer_user.retailer)
   end
 end
 
