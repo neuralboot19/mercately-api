9c5babd90ecb5a3e9b255878650c4fcecfc3c5f2
nintF1link <jalagut8@gmail.com>
2019-03-22 16:04:30 -0500

    fixed slugs

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -1,11 +1,20 @@
 class Retailer < ApplicationRecord
+  has_one :meli_info
   has_many :products, dependent: :destroy
   has_many :customers, dependent: :destroy
   has_many :retailer_users, dependent: :destroy
-  has_one :meli_info
+
   validates :name, presence: true
 
+  after_create :generate_slug
+
   def generate_slug
-    update slug: name.gsub(/\s/, '-')
+    downcased_name = name.downcase
+    if Retailer.find_by(["LOWER(name) LIKE ?","%#{downcased_name}%"])
+      update name: name << "-#{id}"
+      update slug: downcased_name.gsub(/\s/, '-') << "-#{id}"
+    else
+      update slug: downcased_name.gsub(/\s/, '-')
+    end
   end
 end
