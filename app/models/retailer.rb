df4b41246dd7a4399bc070498e619c84123d1f16
nintF1link <jalagut8@gmail.com>
2019-03-25 16:07:27 -0500

    SendGrid and welcome email
    - After Sign up it sends an welcome email
    - Fixed small error for slugs (we need to make tests)

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -10,9 +10,9 @@ class Retailer < ApplicationRecord
   after_create :generate_slug
 
   def generate_slug
-    if Retailer.find_by(['LOWER(name) LIKE ?', "%#{name.downcase}%"])
-      update name: name << "-#{id}"
+    if Retailer.where(['LOWER(name) LIKE ?', "%#{name.downcase}%"]).where.not(id: id).count.positive?
       update slug: name.parameterize << "-#{id}"
+      update name: name << "-#{id}"
     else
       update slug: name.parameterize
     end
