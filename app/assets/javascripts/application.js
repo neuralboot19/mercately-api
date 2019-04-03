fa87d66a9ba4d55cd238b03b35414dcfec4567ae
nintF1link <jalagut8@gmail.com>
2019-04-03 17:15:33 -0500

    Index product

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,51 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 });
+
+function ToastBuilder(options) {
+  // options are optional
+  var opts = options || {};
+
+  // setup some defaults
+  opts.defaultText = opts.defaultText || 'default text';
+  opts.displayTime = opts.displayTime || 7000;
+  opts.target = opts.target || '#content';
+
+  return function (text) {
+    $('<div/>')
+      .addClass('toast')
+      .prependTo($(opts.target))
+      .text(text || opts.defaultText)
+      .queue(function(next) {
+        $(this).css({
+          'opacity': 1
+        });
+        var topOffset = 100;
+        $('.toast').each(function() {
+          var $this = $(this);
+          var height = $this.outerHeight();
+          var offset = 10;
+          $this.css('top', topOffset + 'px');
+
+          topOffset += height + offset;
+        });
+        next();
+      })
+      .delay(opts.displayTime)
+      .queue(function(next) {
+        var $this = $(this);
+        var width = $this.outerWidth() + 20;
+        $this.css({
+          'right': '-' + width + 'px',
+          'opacity': 0
+        });
+        next();
+      })
+      .delay(600)
+      .queue(function(next) {
+        $(this).remove();
+        next();
+      });
+  };
+}
+var showtoast = new ToastBuilder();
