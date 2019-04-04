211e38936eae7610d902b54efd6db98034ff1ffb
nintF1link <jalagut8@gmail.com>
2019-04-04 17:14:50 -0500

    Devise views
    - Forgot password
    - Sign in
    - Sign up
    - Small fixes on landing

--- app/models/retailer_user.rb
+++ app/models/retailer_user.rb
@@ -6,5 +6,15 @@ class RetailerUser < ApplicationRecord
 
   belongs_to :retailer
 
+  before_create :agree_terms_to_bool
+
+  validates :agree_terms, presence: true
+
   accepts_nested_attributes_for :retailer
+
+  protected
+
+    def agree_terms_to_bool
+      ActiveModel::Type::Boolean.new.cast(agree_terms)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_134629) do
+ActiveRecord::Schema.define(version: 2019_04_04_172957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,6 +159,7 @@ ActiveRecord::Schema.define(version: 2019_04_03_134629) do
     t.bigint "retailer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "agree_terms"
     t.index ["email"], name: "index_retailer_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_retailer_users_on_reset_password_token", unique: true
     t.index ["retailer_id"], name: "index_retailer_users_on_retailer_id"
