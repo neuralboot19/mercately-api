4bdb0fd9e7e7db2bce83cc012638b9b0e99c8913
nintF1link <jalagut8@gmail.com>
2019-04-04 08:56:10 -0500

    Fixes

--- db/migrate/20190401160417_add_ancestry_to_categories.rb
+++ db/migrate/20190401160417_add_ancestry_to_categories.rb
@@ -6,7 +6,7 @@ class AddAncestryToCategories < ActiveRecord::Migration[5.2]
     ActiveRecord::Base.transaction do
       faraday_request_get('https://api.mercadolibre.com/sites/MEC/categories').each do |res|
         father = Category.create(meli_id: res['id'], name: res['name'])
-        choticimo(father, res['id'])
+        ml_category_import(father, res['id'])
       end
     end
   end
@@ -18,13 +18,13 @@ class AddAncestryToCategories < ActiveRecord::Migration[5.2]
 
   protected
 
-    def choticimo(father, cat_id)
+    def ml_category_import(father, cat_id)
       meli_url = 'https://api.mercadolibre.com/categories/'
       faraday_request_get("#{meli_url}#{cat_id}")['children_categories'].each do |children_category|
         child = Category.create(meli_id: children_category['id'], name: children_category['name'], parent_id: father.id)
         grandchild_count = faraday_request_get("#{meli_url}#{children_category['id']}")['children_categories'].count
         if grandchild_count > 0
-          choticimo(child, children_category['id'])
+          ml_category_import(child, children_category['id'])
         else
           child
         end
