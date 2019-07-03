34cb6c280367eef9f0c2d8eab35b48293463c760
henry2992 <henry2992@hotmail.com>
2019-07-03 12:04:38 -0500

    Add more data to Order in Active Admin

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,2 +1,17 @@
 ActiveAdmin.register Order do
+
+  show do 
+    default_main_content
+    panel "Usuario de ML" do
+      order_items = Order.find(params['id']).order_items
+      table_for order_items do
+        column :product
+        column :quantity
+        column :created_at
+        column :points
+        column :link
+        column :updated_at
+      end
+    end
+  end
 end
