ecd1f3f0a069e688515db545747e34fcff2e4c8a
henry2992 <henry2992@hotmail.com>
2019-07-10 12:36:53 -0500

    Reverse Proxy for blogs

--- config/application.rb
+++ config/application.rb
@@ -36,10 +36,7 @@ module Mercately
     # Reverse Proxy for Blog
     config.middleware.insert(0, Rack::ReverseProxy) do
       reverse_proxy_options preserve_host: true
-      if Rails.env.production? or Rails.env.staging?
-        reverse_proxy_options force_ssl: true, replace_response_host: true
-      end
-      reverse_proxy /^\/blog(\/?.*)$/, 'https://blog.mercately.com/blog$1'
+      reverse_proxy /^\/blog(\/.*)$/, 'https://blog.mercately.com$1', 
     end
   end
 end
