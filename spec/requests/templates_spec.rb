571c04f10c548d95c39595595a23e7044c7f0a0a
nintF1link <jalagut8@gmail.com>
2019-07-14 09:49:55 -0500

    make rubocop happy

--- spec/requests/templates_spec.rb
+++ spec/requests/templates_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Templates', type: :request, skip: true do
   describe 'GET /templates' do
     it 'works! (now write some real specs)' do
       get templates_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
