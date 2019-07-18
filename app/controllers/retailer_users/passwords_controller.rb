c5a3819991a2aadd890ec6cd74d539a1b2ce00da
Elvis Olivar <ejolivard@gmail.com>
2019-07-18 10:22:40 -0400

    Testing fix on email recovery

--- app/controllers/retailer_users/passwords_controller.rb
+++ app/controllers/retailer_users/passwords_controller.rb
@@ -3,6 +3,15 @@
 class RetailerUsers::PasswordsController < Devise::PasswordsController
   before_action :set_locale
 
+  # POST /resource/password
+  def create
+    super do |resource|
+      if successfully_sent?(resource)
+        flash[:message] = 'Se han enviado las instrucciones al email proporcionado'
+      end
+    end
+  end
+
   protected
 
     def set_locale
