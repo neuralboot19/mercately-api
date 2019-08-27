720d79d7e6957480964bcf6550b7bb3e7f928361
nintF1link <jalagut8@gmail.com>
2019-08-27 10:53:04 -0500

    Redesign v2

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,8 @@ class Message < ApplicationRecord
   belongs_to :customer
   belongs_to :sender, foreign_key: :sender_id, class_name: 'RetailerUser', optional: true
   delegate :retailer_id, :retailer, to: :customer
+
+  def self.latest_message_sent_by(order_id, user_id)
+    where(order_id: order_id, sender_id: user_id).last
+  end
 end
