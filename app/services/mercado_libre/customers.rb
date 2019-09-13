b2f571c46b7a3a49fe9a87dfd694347bec000eb2
Elvis Olivar <ejolivard@gmail.com>
2019-09-13 15:56:29 -0400

    Deleted unnecessary commas

--- app/services/mercado_libre/customers.rb
+++ app/services/mercado_libre/customers.rb
@@ -29,7 +29,12 @@ module MercadoLibre
         customer = find_customer
         return unless customer.present?
 
-        id_type = @exist_order ? @order_params['identification']&.[]('type') : customer_info['identification']&.[]('type')
+        id_type = if @exist_order
+                    @order_params['identification']&.[]('type')
+                  else
+                    customer_info['identification']&.[]('type')
+                  end
+
         id_type.downcase! if id_type.present?
 
         customer.update_attributes!(
@@ -94,36 +99,36 @@ module MercadoLibre
           @nickname = @order_params['nickname']
           @first_name = @order_params['first_name']
           @last_name = @order_params['last_name']
-          @link = @order_params['permalink'],
-          @points = @order_params['points'],
-          @ratings_total = @order_params['seller_reputation']['total'],
-          @transactions_canceled = @order_params['seller_reputation']['transactions']['canceled'],
-          @transactions_completed = @order_params['seller_reputation']['transactions']['completed'],
-          @ratings_neutral = @order_params['seller_reputation']['transactions']['ratings']['neutral'],
-          @ratings_positive = @order_params['seller_reputation']['transactions']['ratings']['positive'],
-          @ratings_negative = @order_params['seller_reputation']['transactions']['ratings']['negative'],
-          @seller_reputation_level_id = @order_params['seller_reputation']['level_id'],
-          @buyer_canceled_transactions = @order_params['buyer_reputation']&.[]('canceled_transactions'),
+          @link = @order_params['permalink']
+          @points = @order_params['points']
+          @ratings_total = @order_params['seller_reputation']['total']
+          @transactions_canceled = @order_params['seller_reputation']['transactions']['canceled']
+          @transactions_completed = @order_params['seller_reputation']['transactions']['completed']
+          @ratings_neutral = @order_params['seller_reputation']['transactions']['ratings']['neutral']
+          @ratings_positive = @order_params['seller_reputation']['transactions']['ratings']['positive']
+          @ratings_negative = @order_params['seller_reputation']['transactions']['ratings']['negative']
+          @seller_reputation_level_id = @order_params['seller_reputation']['level_id']
+          @buyer_canceled_transactions = @order_params['buyer_reputation']&.[]('canceled_transactions')
           @buyer_completed_transactions = @order_params['buyer_reputation']
-            &.[]('transactions')&.[]('completed'),
+            &.[]('transactions')&.[]('completed')
           @buyer_canceled_paid_transactions = @order_params['buyer_reputation']
-            &.[]('transactions')&.[]('canceled')&.[]('paid'),
+            &.[]('transactions')&.[]('canceled')&.[]('paid')
           @buyer_unrated_paid_transactions = @order_params['buyer_reputation']
-            &.[]('transactions')&.[]('unrated')&.[]('paid'),
+            &.[]('transactions')&.[]('unrated')&.[]('paid')
           @buyer_unrated_total_transactions = @order_params['buyer_reputation']
-            &.[]('transactions')&.[]('unrated')&.[]('total'),
+            &.[]('transactions')&.[]('unrated')&.[]('total')
           @buyer_not_yet_rated_paid_transactions = @order_params['buyer_reputation']
-            &.[]('transactions')&.[]('not_yet_rated')&.[]('paid'),
+            &.[]('transactions')&.[]('not_yet_rated')&.[]('paid')
           @buyer_not_yet_rated_total_transactions = @order_params['buyer_reputation']
-            &.[]('transactions')&.[]('not_yet_rated')&.[]('total'),
-          @meli_registration_date = @order_params['registration_date'],
-          @phone_area = @order_params['phone']&.[]('area_code'),
+            &.[]('transactions')&.[]('not_yet_rated')&.[]('total')
+          @meli_registration_date = @order_params['registration_date']
+          @phone_area = @order_params['phone']&.[]('area_code')
           @phone_verified = @order_params['phone']&.[]('verified')
-          @id_number = @order_params['identification']&.[]('number'),
-          @address = @order_params['address']&.[]('address'),
-          @city = @order_params['address']&.[]('city'),
-          @state = @order_params['address']&.[]('state'),
-          @zip_code = @order_params['address']&.[]('zip_code'),
+          @id_number = @order_params['identification']&.[]('number')
+          @address = @order_params['address']&.[]('address')
+          @city = @order_params['address']&.[]('city')
+          @state = @order_params['address']&.[]('state')
+          @zip_code = @order_params['address']&.[]('zip_code')
           @country_id = @order_params['country_id']
         else
           @email = customer_info['email']
@@ -131,36 +136,36 @@ module MercadoLibre
           @nickname = customer_info['nickname']
           @first_name = customer_info['first_name']
           @last_name = customer_info['last_name']
-          @link = customer_info['permalink'],
-          @points = customer_info['points'],
-          @ratings_total = customer_info['seller_reputation']['total'],
-          @transactions_canceled = customer_info['seller_reputation']['transactions']['canceled'],
-          @transactions_completed = customer_info['seller_reputation']['transactions']['completed'],
-          @ratings_neutral = customer_info['seller_reputation']['transactions']['ratings']['neutral'],
-          @ratings_positive = customer_info['seller_reputation']['transactions']['ratings']['positive'],
-          @ratings_negative = customer_info['seller_reputation']['transactions']['ratings']['negative'],
-          @seller_reputation_level_id = customer_info['seller_reputation']['level_id'],
-          @buyer_canceled_transactions = customer_info['buyer_reputation']&.[]('canceled_transactions'),
+          @link = customer_info['permalink']
+          @points = customer_info['points']
+          @ratings_total = customer_info['seller_reputation']['total']
+          @transactions_canceled = customer_info['seller_reputation']['transactions']['canceled']
+          @transactions_completed = customer_info['seller_reputation']['transactions']['completed']
+          @ratings_neutral = customer_info['seller_reputation']['transactions']['ratings']['neutral']
+          @ratings_positive = customer_info['seller_reputation']['transactions']['ratings']['positive']
+          @ratings_negative = customer_info['seller_reputation']['transactions']['ratings']['negative']
+          @seller_reputation_level_id = customer_info['seller_reputation']['level_id']
+          @buyer_canceled_transactions = customer_info['buyer_reputation']&.[]('canceled_transactions')
           @buyer_completed_transactions = customer_info['buyer_reputation']
-            &.[]('transactions')&.[]('completed'),
+            &.[]('transactions')&.[]('completed')
           @buyer_canceled_paid_transactions = customer_info['buyer_reputation']
-            &.[]('transactions')&.[]('canceled')&.[]('paid'),
+            &.[]('transactions')&.[]('canceled')&.[]('paid')
           @buyer_unrated_paid_transactions = customer_info['buyer_reputation']
-            &.[]('transactions')&.[]('unrated')&.[]('paid'),
+            &.[]('transactions')&.[]('unrated')&.[]('paid')
           @buyer_unrated_total_transactions = customer_info['buyer_reputation']
-            &.[]('transactions')&.[]('unrated')&.[]('total'),
+            &.[]('transactions')&.[]('unrated')&.[]('total')
           @buyer_not_yet_rated_paid_transactions = customer_info['buyer_reputation']
-            &.[]('transactions')&.[]('not_yet_rated')&.[]('paid'),
+            &.[]('transactions')&.[]('not_yet_rated')&.[]('paid')
           @buyer_not_yet_rated_total_transactions = customer_info['buyer_reputation']
-            &.[]('transactions')&.[]('not_yet_rated')&.[]('total'),
-          @meli_registration_date = customer_info['registration_date'],
-          @phone_area = customer_info['phone']&.[]('area_code'),
+            &.[]('transactions')&.[]('not_yet_rated')&.[]('total')
+          @meli_registration_date = customer_info['registration_date']
+          @phone_area = customer_info['phone']&.[]('area_code')
           @phone_verified = customer_info['phone']&.[]('verified')
-          @id_number = customer_info['identification']&.[]('number'),
-          @address = customer_info['address']&.[]('address'),
-          @city = customer_info['address']&.[]('city'),
-          @state = customer_info['address']&.[]('state'),
-          @zip_code = customer_info['address']&.[]('zip_code'),
+          @id_number = customer_info['identification']&.[]('number')
+          @address = customer_info['address']&.[]('address')
+          @city = customer_info['address']&.[]('city')
+          @state = customer_info['address']&.[]('state')
+          @zip_code = customer_info['address']&.[]('zip_code')
           @country_id = customer_info['country_id']
         end
       end
