8d67d7f307980c64a8de447a4f71f6d7f60337d6
Elvis Olivar <ejolivard@gmail.com>
2019-09-18 12:13:22 -0400

    Deleted unnecessary code in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,16 +6,4 @@ module ApplicationHelper
       "#{date.day} - #{date.strftime('%b')} (hace #{time_ago_in_words(date)})"
     end
   end
-
-  def total_unread
-    quantity_unread_messages + quantity_unread_questions
-  end
-
-  def quantity_unread_messages
-    Retailer.messages_total(current_retailer_user.retailer.id)
-  end
-
-  def quantity_unread_questions
-    Retailer.questions_total(current_retailer_user.retailer.id)
-  end
 end
