6527ce874571c1e5c421e21770237491a90cda51
dortin <jalagut8@gmail.com>
2019-10-01 14:20:00 -0500

    Sentry to hola@mercately.com

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,8 @@ module Mercately
     end
 
     Raven.configure do |config|
-      config.dsn = 'https://6be9b8318da648a6acdb5504bcf04bbf:1277be5f12c64794ac4bf3f6e88997fd@sentry.io/1386758'
-      config.environments = ['staging', 'production']
+      config.dsn = 'https://d83a4c9f783e44a0871416e565f38e4a:e73cd5c4fde44f909bbef10fd9d050aa@sentry.io/1767402'
+      config.environments = %w(staging production)
     end
 
     # Settings in config/environments/* take precedence over those specified here.
@@ -36,7 +36,7 @@ module Mercately
     # Reverse Proxy for Blog
     config.middleware.insert(0, Rack::ReverseProxy) do
       reverse_proxy_options preserve_host: true
-      reverse_proxy /^\/blog(\/.*)$/, 'https://blog.mercately.com$1'
+      reverse_proxy(/^\/blog(\/.*)$/, 'https://blog.mercately.com$1')
     end
   end
 end
