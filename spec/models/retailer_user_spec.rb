849789c4c423600957acf485425329bff0e8aaf9
Elvis Olivar <ejolivard@gmail.com>
2019-10-02 16:42:18 -0400

    Added unit tests to retailer user model

--- spec/models/retailer_user_spec.rb
+++ spec/models/retailer_user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe RetailerUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:retailer) }
+    it { is_expected.to accept_nested_attributes_for(:retailer) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:agree_terms) }
+  end
+
+  describe 'protected/private methods' do
+    let(:retailer_user) { create(:retailer_user) }
+
+    it 'should return true for agree terms converter' do
+      expect(retailer_user.send(:agree_terms_to_bool)).to be_truthy
+    end
+
+    it 'should send a welcome email' do
+      expect(retailer_user.send(:send_welcome_email)).to be_a Mail::Message
+    end
+  end
 end
