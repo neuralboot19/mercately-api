9876dbf3d15ac47f0fc90df746246793071a8e8a
Elvis Olivar <ejolivard@gmail.com>
2019-10-03 15:42:28 -0400

    Deleted duplicated for subject on test units

--- spec/mailers/retailer_mailer_spec.rb
+++ spec/mailers/retailer_mailer_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe RetailerMailer, type: :mailer do
   describe '.welcome' do
     subject(:retailer_user) { create(:retailer_user, email: 'retailer@example.com') }
-    subject(:mail) { described_class.welcome(retailer_user).deliver_now }
+
+    let(:mail) { described_class.welcome(retailer_user).deliver_now }
 
     it 'shows the correct subject to retailer' do
       expect(mail.subject).to eq('Bienvenido a Mercately')
