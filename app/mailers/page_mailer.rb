5bfe1b232b9ce023e6f498e197d1ce343c268a85
Elvis Olivar <ejolivard@gmail.com>
2019-10-04 08:07:40 -0400

    Deleted useless file

--- app/mailers/page_mailer.rb
+++
@@ -1,7 +0,0 @@
-class PageMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.page_mailer.welcome.subject
-  #
-end
