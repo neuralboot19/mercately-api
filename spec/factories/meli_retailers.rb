88a6ce1967597b8f8cab6fd866dde24f8b2a8f39
Elvis Olivar <ejolivard@gmail.com>
2019-10-04 11:45:05 -0400

    Added unit tests to meli retailer

--- spec/factories/meli_retailers.rb
+++ spec/factories/meli_retailers.rb
@@ -1,25 +1,24 @@
 FactoryBot.define do
   factory :meli_retailer do
-    access_token {}
-    meli_user_id {}
-    refresh_token {}
-    retailer_id {}
-    nickname {}
-    email {}
-    points {}
-    link {}
-    seller_experience {}
-    seller_reputation_level_id {}
-    transactions_canceled {}
-    transactions_completed {}
-    ratings_negative {}
-    ratings_neutral {}
-    ratings_positive {}
-    ratings_total {}
-    customer_id {}
-    phone {}
-    has_meli_info {}
-    meli_token_updated_at {}
-    meli_info_updated_at {}
+    access_token { Faker::Internet.uuid }
+    meli_user_id { '123456789' }
+    refresh_token { Faker::Internet.uuid }
+    retailer
+    nickname { Faker::Internet.username }
+    email { Faker::Internet.email }
+    points { Faker::Number.digit }
+    link { Faker::Internet.url }
+    seller_experience { Faker::Verb.base }
+    seller_reputation_level_id { Faker::Verb.base }
+    transactions_canceled { Faker::Number.digit }
+    transactions_completed { Faker::Number.digit }
+    ratings_negative { Faker::Number.digit }
+    ratings_neutral { Faker::Number.digit }
+    ratings_positive { Faker::Number.digit }
+    ratings_total { Faker::Number.digit }
+    phone { Faker::PhoneNumber.phone_number }
+    has_meli_info { true }
+    meli_token_updated_at { Time.now }
+    meli_info_updated_at { Time.now }
   end
 end
--- spec/models/meli_retailer_spec.rb
+++ spec/models/meli_retailer_spec.rb
@@ -1,11 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe MeliRetailer, type: :model do
+  subject(:meli_retailer) { build(:meli_retailer) }
+
   describe 'associations' do
     it { is_expected.to belong_to(:retailer) }
   end
 
   describe '.check_unique_retailer_id' do
-    
+    context 'when it does not exist yet' do
+      it 'let the meli_retailer creation' do
+        expect(MeliRetailer.check_unique_retailer_id(meli_retailer.meli_user_id)).to be false
+      end
+    end
+
+    context 'when it already exists' do
+      it 'rejects the meli_retailer creation' do
+        meli_retailer.save
+        expect(MeliRetailer.check_unique_retailer_id(meli_retailer.meli_user_id)).to be true
+      end
+    end
+  end
+
+  describe '#update_information' do
+    context 'when is a new record' do
+      it 'calls the ML updating service' do
+        meli_retailer.meli_info_updated_at = nil
+        expect(meli_retailer.send(:update_information)).to be_a MeliRetailer
+      end
+    end
+
+    context 'when it has not been updated in more than 7 days' do
+      it 'calls the ML updating service' do
+        meli_retailer.meli_info_updated_at = Time.now - 8.days
+        expect(meli_retailer.send(:update_information)).to be_a MeliRetailer
+      end
+    end
+
+    context 'when it has been updated in less than 7 days' do
+      it 'does not call the ML updating service' do
+        meli_retailer.meli_info_updated_at = Time.now - 5.days
+        expect(meli_retailer.send(:update_information)).to be_nil
+      end
+    end
   end
 end
