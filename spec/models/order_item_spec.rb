ea347727d1e6fa6e40c0b02781b21c0ae85f7a44
Elvis Olivar <ejolivard@gmail.com>
2019-10-08 10:35:05 -0400

    Added unit tests to associations in order item

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:order_item) { build(:order_item) }
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:order).inverse_of(:order_items) }
+    it { is_expected.to belong_to(:product) }
+    it { is_expected.to belong_to(:product_variation).required(false) }
+  end
+
+  describe 'delegates' do
+    it { is_expected.to delegate_method(:meli_product_id).to(:product) }
+  end
+
+  describe '#subtotal' do
+    it 'calculates the item quantity multilplied by unit price' do
+      expect(order_item.subtotal).to eq(order_item.quantity * order_item.unit_price)
+    end
+  end
 end
