536cf9f64185f1580596961784543abe0754b6f7
dortin <jalagut8@gmail.com>
2019-10-18 09:51:47 -0500

    Test request demo feature

---
+++ spec/mailers/request_demo_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe RequestDemoMailer, type: :mailer do
+  describe '#demo_requested' do
+    subject(:mail) { RequestDemoMailer.demo_requested(client_data) }
+
+    let(:client_data) do
+      {
+        name: 'John Doe',
+        email: 'john@doe.com',
+        company: 'Acme Inc',
+        phone: '098-333-4444',
+        messaje: 'Hi, I\'d like a demo',
+      }
+    end
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq("Mercately demo requested by #{client_data[:name]}")
+      expect(mail.to).to eq(['hola@mercately.com', 'henry2992@hotmail.com', 'pvelasquez9294@gmail.com', 'jalagut8@gmail.com'])
+      expect(mail.from).to eq(['hola@mercately.com'])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match(client_data[:name])
+      expect(mail.body.encoded).to match(client_data[:email])
+      expect(mail.body.encoded).to match(client_data[:company])
+      expect(mail.body.encoded).to match(client_data[:phone])
+      expect(mail.body.encoded).to match(client_data[:messaje])
+    end
+  end
+end
---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Pages', type: :request do
+  describe 'POST #request_demo' do
+    it 'sends an email to admins and shows the index' do
+      get '/'
+      expect(response).to render_template(:index)
+
+      post '/request_demo', params: { name: 'John Doe', email: 'john@doe.com', phone: '098-333-4444' }
+      expect(ActionMailer::Base.deliveries.size).to eq 1
+      expect(response).to redirect_to('/')
+
+      follow_redirect!
+      expect(response).to render_template(:index)
+      expect(response.body).to include('Gracias por la solicitud, pronto nos comunicaremos contigo')
+    end
+  end
+end
