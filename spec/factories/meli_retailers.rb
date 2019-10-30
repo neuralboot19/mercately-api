e563965be414f9405b5285b3dbac485659f5f48e
dortin <jalagut8@gmail.com>
2019-10-30 11:55:11 -0500

    Test message service

--- spec/factories/meli_retailers.rb
+++ spec/factories/meli_retailers.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :meli_retailer do
     retailer
-    access_token { 'APP_USR-8515522317464897-102316-b08a6117d32b784bbdc889fbcfa5fdb5-472964088' }
+    access_token { 'APP_USR-8515522317464897-102919-8c427f047480b1d0c80321ce26dd60f5-425933549' }
     refresh_token { Faker::Internet.uuid }
     nickname { Faker::Internet.username }
     email { Faker::Internet.email }
---
+++ spec/services/mercado_libre/messages_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+require 'vcr'
+
+RSpec.describe MercadoLibre::Messages, vcr: true do
+  subject(:messages_service) { described_class.new(retailer) }
+
+  let(:retailer) { meli_retailer.retailer }
+  let(:meli_retailer) { create(:meli_retailer) }
+  let(:meli_customer) { create(:meli_customer) }
+  let(:customer) { create(:customer, retailer: retailer, meli_customer: meli_customer) }
+  let!(:order) { create(:order, meli_order_id: '2194381783', customer: customer) }
+
+  describe '#import' do
+    it 'saves the message' do
+      VCR.use_cassette('messages/message_readed') do
+        expect { messages_service.import('a1f4ddcaa5874871b00b3baaa02cf245') }.to change(Message, :count).by(1)
+      end
+    end
+
+    context 'when the message is sent from ML' do
+      it 'saves the message' do
+        VCR.use_cassette('messages/message_from_retailer') do
+          expect { messages_service.import('935039e5a90f4f428b7051931af04a0b') }.to change(Message, :count).by(1)
+        end
+      end
+    end
+  end
+
+  describe '#answer_message' do
+    let(:message) { create(:message, order: order, customer: customer) }
+    before do
+      stub_const('Response', Struct.new(:status, :body))
+      response = Response.new(201, '{"status": "success", "message": "My answer message"}')
+      allow(Connection).to receive(:post_request)
+        .with(anything, anything).and_return(response)
+    end
+
+    it 'sends a message for that order in ML' do
+      response = messages_service.answer_message(message)
+      expect(response).to have_key('message')
+    end
+  end
+end
