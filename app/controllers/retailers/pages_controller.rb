67b135d4b19f3ff859ade88584a2549bd3254d60
Elvis Olivar <ejolivard@gmail.com>
2019-12-04 09:27:28 -0400

    Applied range of dates to dasboard

--- app/controllers/retailers/pages_controller.rb
+++ app/controllers/retailers/pages_controller.rb
@@ -10,17 +10,20 @@ class Retailers::PagesController < RetailersController
     @orders_range = @orders.range_between(@start_date, @end_date)
     @orders_count = @orders_range.count
     @customers = current_retailer.customers.range_between(@start_date, @end_date)
-    @orders_total = @orders.count
-    @profit_total = @orders.sum { |ord| ord.total_amount || 0 }
-    @success_orders_count = Order.success.where(customer_id: current_retailer.customers.ids).count
-    @pending_orders_count = Order.pending.where(customer_id: current_retailer.customers.ids).count
-    @cancelled_orders_count = Order.cancelled.where(customer_id: current_retailer.customers.ids).count
-    @incoming_total = @orders_range.group_by_day(:created_at).sum(:total_amount)
+    @profit_total = @orders_range.success.sum { |ord| ord.total_amount || 0 }
+    @success_orders_count = @orders_range.success.where(customer_id: current_retailer.customers.ids).count
+    @pending_orders_count = @orders_range.pending.where(customer_id: current_retailer.customers.ids).count
+    @cancelled_orders_count = @orders_range.cancelled.where(customer_id: current_retailer.customers.ids).count
+    @incoming_total = @orders_range.success.group_by_day(:created_at).sum(:total_amount)
     @better_products = current_retailer.products.joins(:orders).group('products.id, orders.id')
       .order('COUNT(orders.id)').limit(5).map { |p| { name: p.title, data: p.orders.group_by_day(:created_at).count } }
     @better_customers = @customers.joins(:orders).group('customers.id, orders.id')
       .order('COUNT(orders.id)').limit(5).map { |c| [c.full_name, c.orders.count] }
-    @total_chats = Message.where(customer_id: current_retailer.customers.ids, answer: nil).count
-    @total_questions = Question.where(customer_id: current_retailer.customers.ids).count
+    @chats = Message.where(customer_id: current_retailer.customers.ids, answer: nil)
+    @total_chats = @chats.range_between(@start_date, @end_date)
+      .where(customer_id: current_retailer.customers.ids, answer: nil).count
+    @questions = Question.where(customer_id: current_retailer.customers.ids)
+    @total_questions = @questions.range_between(@start_date, @end_date)
+      .where(customer_id: current_retailer.customers.ids).count
   end
 end
