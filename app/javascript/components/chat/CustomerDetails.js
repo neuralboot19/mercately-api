ecfaeb69308552c89482d178d3084fea585e1e66
henry2992 <henry2992@hotmail.com>
2019-12-11 13:44:29 -0500

    WIP

--- app/javascript/components/chat/CustomerDetails.js
+++ app/javascript/components/chat/CustomerDetails.js
@@ -12,7 +12,62 @@ class CustomerDetails extends Component {
     let customer = this.props.customerDetails
     return (
       <div className="customer_sidebar">
-        <p>{customer.first_name} {customer.last_name}</p>
+
+        <div className="customer_box">
+          <p>
+            {customer.first_name} {customer.last_name}
+            <i class="fs-18 mt-4 mr-4 f-right far fa-external-link-alt"></i>
+          </p>
+        </div>
+
+        <div className="customer_details">
+
+          <div className="details_holder">
+            <span>Detalles</span>
+          </div>
+          
+          <div>
+            <p className="label">Teléfono:</p>
+            <i class="fs-18 mt-4 mr-4 fab fa-whatsapp-square"><span className="tag">+593996779124</span></i>
+          </div>
+
+          <div>
+            <p className="label">Email:</p>
+            <i class="fs-18 mt-4 mr-4 fas fa-envelope-square"><span className="tag">henry2992@hotmail.com</span></i>
+          </div>
+
+          <div>
+            <p className="label">Facebook:</p>
+            <i class="fs-18 mt-4 mr-4 fab fa-facebook-square"><span className="tag">henry2992@hotmail.com</span></i>
+          </div>
+
+        </div>
+
+        <div className="customer_details">
+
+          <div className="details_holder">
+            <span>Detalles</span>
+          </div>
+          
+          <div>
+            <p className="label">Teléfono:</p>
+            <i class="fs-18 mt-4 mr-4 fab fa-whatsapp-square"><span className="tag">+593996779124</span></i>
+          </div>
+
+          <div>
+            <p className="label">Email:</p>
+            <i class="fs-18 mt-4 mr-4 fas fa-envelope-square"><span className="tag">henry2992@hotmail.com</span></i>
+          </div>
+
+          <div>
+            <p className="label">Facebook:</p>
+            <i class="fs-18 mt-4 mr-4 fab fa-facebook-square"><span className="tag">henry2992@hotmail.com</span></i>
+          </div>
+
+        </div>
+
+
+
       </div>
     )
   }
