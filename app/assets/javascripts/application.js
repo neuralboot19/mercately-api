d4fd9998a1a4c41935f1277416c5b3d2f03b3b38
Elvis Olivar <ejolivard@gmail.com>
2020-01-09 14:50:23 -0400

    Required moment spanish language

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require chartkick
 //= require Chart.bundle
 //= require moment
+//= require moment/locale/es
 //= require daterangepicker
 //= require cable
 //= require channels/counter_messaging_channel
