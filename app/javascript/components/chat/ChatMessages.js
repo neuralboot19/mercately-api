70941d05682717d078020f1ad67cc189a60d5eeb
dortin <jalagut8@gmail.com>
2020-01-31 15:54:30 -0500

    Remove debugger and improve indent

--- app/javascript/components/chat/ChatMessages.js
+++ app/javascript/components/chat/ChatMessages.js
@@ -79,13 +79,12 @@ class ChatMessages extends Component {
         received: data => {
           var facebook_message = data.facebook_message;
           if (currentCustomer == facebook_message.customer_id) {
-            debugger;
-              if (!this.state.new_message) {
-                this.setState({
-                  messages: this.state.messages.concat(facebook_message),
-                  new_message: false,
-                })
-              }
+            if (!this.state.new_message) {
+              this.setState({
+                messages: this.state.messages.concat(facebook_message),
+                new_message: false,
+              })
+            }
             this.props.setMessageAsReaded(facebook_message.id, csrfToken);
           }
         }
