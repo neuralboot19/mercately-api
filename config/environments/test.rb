63a93948cd1fbedba44c20a45d477897c58004b6
Johnmer Bencomo <bjohnmer@gmail.com>
2020-03-18 18:15:45 -0500

    Return true if Rails.env is production when running AddPackInfoToOrders migration

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,4 +46,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.default_url_options = { host: "localhost:3000" }
+
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
 end
--- db/migrate/20200309143611_add_pack_info_to_orders.rb
+++ db/migrate/20200309143611_add_pack_info_to_orders.rb
@@ -1,11 +1,13 @@
 class AddPackInfoToOrders < ActiveRecord::Migration[5.2]
   def up
+    return true if Rails.env == 'test'
     unless Order.column_names.include?('pack_id')
       add_column :orders, :pack_id, :string
     end
   end
 
   def down
+    return true if Rails.env == 'test'
     remove_column :orders, :pack_id, :string
   end
 end
