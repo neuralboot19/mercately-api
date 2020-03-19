48325a08578b2f493745b94995e5176898f8dc8f
Johnmer Bencomo <bjohnmer@gmail.com>
2020-03-19 12:10:47 -0500

    Kickback #1: Fixed url and changed customer_id param by phone_number, also limit the success params to show in the response

--- app/controllers/retailers/api/v1/karix_whatsapp_controller.rb
+++ app/controllers/retailers/api/v1/karix_whatsapp_controller.rb
@@ -1,21 +1,38 @@
 module Retailers::Api::V1
   class KarixWhatsappController < Retailers::Api::V1::ApiController
+    KARIX_PERMITED_PARAMS = %w[
+      channel
+      content
+      direction
+      status
+      destination
+      country
+      created_time
+      error
+    ].freeze
+
     def create
-      return record_not_found unless params[:customer_id].present?
-      return render_unauthorized unless params[:message].present?
+      params_present = params[:phone_number].present? && params[:message].present?
+      set_response(500, 'Error: Missing phone number and/or message') && return unless params_present
 
-      customer = current_retailer.customers.find(params[:customer_id])
       karix_helper = KarixNotificationHelper
-      response = karix_helper.ws_message_service.send_message(current_retailer, customer, params, 'text')
+      response = karix_helper.ws_message_service.send_message(current_retailer, nil, params, 'text')
 
-      set_response(500, 'Error', response['objects'][0]['error'].to_json) && return if response['objects'][0]['status'] == 'failed'
+      error = response['objects'][0]['status'] == 'failed'
+      set_response(500, 'Error', response['objects'][0]['error'].to_json) && return if error
 
       message = current_retailer.karix_whatsapp_messages.find_or_initialize_by(uid: response['objects'][0]['uid'])
       message = karix_helper.ws_message_service.assign_message(message, current_retailer, response['objects'][0])
       message.save
 
       karix_helper.broadcast_data(current_retailer, message)
-      set_response(200, 'Ok',response['objects'][0].to_json)
+      set_response(200, 'Ok', format_response(response['objects'][0]))
     end
+
+    private
+
+      def format_response(object)
+        object.slice(*KARIX_PERMITED_PARAMS).to_json
+      end
   end
 end
