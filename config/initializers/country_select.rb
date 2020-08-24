cdfbcec2c2a4236f36163fb15c5724b415ff6947
Elvis Olivar <ejolivard@gmail.com>
2020-08-24 11:17:21 -0400

    Added the flags to countries

--- config/initializers/country_select.rb
+++ config/initializers/country_select.rb
@@ -1,3 +1,3 @@
 CountrySelect::FORMATS[:with_country_code] = lambda do |country|
-  "#{country.translations[I18n.locale.to_s] || country.name} (+#{country.country_code})"
+  "#{country.emoji_flag} #{country.translations[I18n.locale.to_s] || country.name} (+#{country.country_code})"
 end
