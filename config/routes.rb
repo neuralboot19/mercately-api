1777cc0a662da8b25ac6bbc5894329a2a7fb4509
Elvis Olivar <ejolivard@gmail.com>
2021-03-03 18:11:01 -0400

    Added endpoint to retrieve messenger conversations

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,7 @@ Rails.application.routes.draw do
         get 'agents', to: 'retailer_users#index'
         get 'whatsapp_templates', to: 'whatsapp_templates#index'
         get 'whatsapp_conversations', to: "whatsapp_conversations#whatsapp_conversations"
+        get 'messenger_conversations', to: 'messenger_conversations#messenger_conversations'
 
         resources :customers, only: [:update]
       end
