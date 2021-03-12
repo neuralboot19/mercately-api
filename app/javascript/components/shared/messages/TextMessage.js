1d01c6b0d280cbb1cd4e3f9ebf706bcf8e19a71c
dortin42 <jalagut8@gmail.com>
2021-03-12 16:34:22 -0500

    feat: parse messages regarding WA format

--- app/javascript/components/shared/messages/TextMessage.js
+++ app/javascript/components/shared/messages/TextMessage.js
@@ -2,12 +2,17 @@ import React from 'react';
 import MessageStatus from '../MessageStatus';
 
 const TextMessage = ({ chatType, handleMessageEvents, message }) => {
-  const messageText = chatType === "whatsapp" ? message.content_text : message.text;
+  const formatText = (text) => text.replace(/~([^~\n]*[^~\s])~/g, '<s>$1</s>')
+    .replace(/_([^_\n]*[^_\s])_/g, '<i>$1</i>')
+    .replace(/\*([^*\n]*[^*\s])\*/g, '<b>$1</b>')
+    .replace(/`{3}([^`\n]*[^`\s])`{3}/g, '<pre class="d-inline-block">$1</pre>');
+
+  let messageText = chatType === "whatsapp" ? message.content_text : message.text;
+  messageText = formatText(messageText);
 
   return (
     <div className="text-pre-line">
-      {messageText}
-      <br />
+      <div dangerouslySetInnerHTML={{ __html: messageText }} />
       <MessageStatus
         chatType={chatType}
         handleMessageEvents={handleMessageEvents}
--- app/javascript/components/shared/messages/replied/ReplyTextMessage.js
+++ app/javascript/components/shared/messages/replied/ReplyTextMessage.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-
 const ReplyTextMessage = ({ text }) => {
+  const formatText = (text) => text.replace(/~([^~\n]*[^~\s])~/g, '<s>$1</s>')
+    .replace(/_([^_\n]*[^_\s])_/g, '<i>$1</i>')
+    .replace(/\*([^*\n]*[^*\s])\*/g, '<b>$1</b>')
+    .replace(/`{3}([^`\n]*[^`\s])`{3}/g, '<pre class="d-inline-block">$1</pre>');
+
+  const messageText = formatText(text);
 
   return (
-    <span
-      className="text text-pre-line">{text}</span>
-  )
-}
+    <span className="text text-pre-line">
+      {messageText}
+    </span>
+  );
+};
 
 export default ReplyTextMessage;
