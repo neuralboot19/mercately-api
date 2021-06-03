50f9300ae271117e364e763ee8dc1d35c25253db
Henry <henry2992@hotmail.com>
2021-06-03 18:31:40 -0500

    Cambiar ventas por ordenes

--- app/helpers/customer_helper.rb
+++ app/helpers/customer_helper.rb
@@ -5,8 +5,8 @@ module CustomerHelper
        'first_name desc', label: 'Nombres Desc' }, { value: 'email asc', label: 'Email Asc' }, { value:
        'email desc', label: 'Email Desc' }, { value: 'phone asc', label: 'Teléfono Asc' }, { value:
        'phone desc', label: 'Teléfono Desc' }, { value: 'sort_by_completed_orders desc', label:
-       'Ventas completadas' }, { value: 'sort_by_pending_orders desc', label: 'Ventas pendientes' }, { value:
-       'sort_by_canceled_orders desc', label: 'Ventas canceladas' }, { value: 'sort_by_total asc', label:
+       'Órdenes completadas' }, { value: 'sort_by_pending_orders desc', label: 'Órdenes pendientes' }, { value:
+       'sort_by_canceled_orders desc', label: 'Órdenes canceladas' }, { value: 'sort_by_total asc', label:
        'Total Asc' }, { value: 'sort_by_total desc', label: 'Total Desc' }]
   end
 
