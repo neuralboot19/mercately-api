e81eeff58e1234c42ba3729213ca7f5d8e4800a4
dortin42 <jalagut8@gmail.com>
2021-09-29 15:01:31 -0500

    hotfix: dont send errors to slack if disabled or Slack service down

--- app/services/slack_error.rb
+++ app/services/slack_error.rb
@@ -1,16 +1,20 @@
 class SlackError
   def self.send_error(e)
-    return if ENV['ENVIRONMENT'] != 'production'
+    return if ENV['ENVIRONMENT'] != 'production' || ENV['SLACK_DEBUG'].nil?
 
     slack_client.ping([
       "Error: #{e.message}",
       "Backtrace: #{e.backtrace.select { |x| x.match(/app/) }}"
     ].join("\n"))
   rescue
-    slack_client.ping([
-      "Error: #{e}",
-      "Backtrace: #{caller[0]}"
-    ].join("\n"))
+    begin
+      slack_client.ping([
+        "Error: #{e}",
+        "Backtrace: #{caller[0]}"
+      ].join("\n"))
+    rescue
+      Rails.logger.error('Slack disabled')
+    end
   end
 
   def self.slack_client
