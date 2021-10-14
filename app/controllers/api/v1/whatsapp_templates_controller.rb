7fa9b4efcfc502a3f01b3568805b235c41a52246
Sergio Leon <efrain.loenh.trabajo@gmail.com>
2021-10-14 12:05:56 -0500

    added space

--- app/controllers/api/v1/whatsapp_templates_controller.rb
+++ app/controllers/api/v1/whatsapp_templates_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::WhatsappTemplatesController < Api::ApiController
     @templates = current_retailer.whatsapp_templates.active
 
     if @templates.present?
-      render status: 200, json: { templates: @templates}
+      render status: 200, json: { templates: @templates }
     else
       render status: 404, json: { message: 'No existen plantillas' }
     end
