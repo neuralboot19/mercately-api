2227237fd0b124a1fa35223dcb8083b2f8b25917
Guillermo Ramirez <muriet96@gmail.com>
2021-11-17 17:31:10 -0500

    feat: Add retailer_id to agent_serializer.rb

--- app/serializers/retailers/api/v1/agent_serializer.rb
+++ app/serializers/retailers/api/v1/agent_serializer.rb
@@ -1,6 +1,6 @@
 module Retailers::Api::V1
   class AgentSerializer < ActiveModel::Serializer
-    attributes :id, :first_name, :last_name, :email, :admin
+    attributes :id, :first_name, :last_name, :email, :admin, :retailer_id
 
     def admin
       object.retailer_admin
