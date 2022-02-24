f2f23faf0e1e435f4389009d4d34da0571c5ac56
Elvis Olivar <ejolivard@gmail.com>
2022-02-24 14:58:50 -0400

    Added alert to retailers with negative Ws balance

--- app/admin/payment_plans.rb
+++ app/admin/payment_plans.rb
@@ -1,4 +1,4 @@
 ActiveAdmin.register PaymentPlan do
   permit_params :price, :start_date, :next_pay_date, :status, :plan, :karix_available_messages,
-                :karix_available_notifications, :month_interval
+                :karix_available_notifications, :month_interval, :charge_attempt
 end
